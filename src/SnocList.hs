236c3ee37279e5a0861c78035859b76b9567bc42
Daniel Aragon <daniel.aragon@ciencias.unam.mx>
2018-08-16 22:35:19 -0500

    Added initS

--- src/SnocList.hs
+++ src/SnocList.hs
@@ -1,6 +1,6 @@
 {-|
 Module      : ListS
-Description : Scno lists Representation datatype with a few useful functions built around it.
+Description : Snoc lists representation with a few useful functions built around it.
 License     : Apache License 2.0
 Maintainer  : daniel.aragon@ciencias.unam.mx
 -}
@@ -8,7 +8,8 @@ Maintainer  : daniel.aragon@ciencias.unam.mx
 module ListS(
   ListS,
   headS,
-  tailS
+  tailS,
+  initS
 ) where
 
 -- |Snoc list datatype. NilS represent the empty list, and Snoc represents the operator that
@@ -19,7 +20,7 @@ data ListS a = NilS | Snoc (ListS a) a deriving Show
 -- |This function retrieves the first element of the list.
 headS :: ListS a -> a
 headS list = case list of
-  NilS -> error "Empty List"
+  NilS -> error "Empty list"
   (Snoc list' x) -> case list' of
     NilS -> x
     _    -> headS list'
@@ -27,7 +28,13 @@ headS list = case list of
 -- |This function retrieves the list without the first element.
 tailS :: ListS a -> ListS a
 tailS list = case list of
-  NilS -> error "Empty List"
+  NilS -> error "Empty list"
   (Snoc list' x) -> case list' of
     NilS -> NilS
     _    -> Snoc (tailS list') x
+
+-- |This function retrieves the list without the last element.
+initS :: ListS a -> ListS a
+initS list = case list of
+  NilS           -> error "Empty list"
+  (Snoc list' _) -> list'
